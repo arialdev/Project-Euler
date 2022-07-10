acdc3233222ac45ac62e9eba7e5eb356ee4866d3
Álvaro <alvarora12@gmail.com>
2022-07-10 21:52:32 +0200

    moved js project and improve ex 10

--- js/problems/10.js
+++ js/problems/10.js
@@ -1,10 +1,12 @@
+import { cpus } from 'node:os'
+
 export async function getPrimesSum(upperBound) {
-  const THREADS_COUNT = 40;
+  const THREADS_COUNT = cpus().length;
   const BATCH_SIZE = Math.floor(upperBound / THREADS_COUNT);
   const promises = [];
   for (let i = 0; i < THREADS_COUNT; i++) {
     const lb = (i * BATCH_SIZE) || 2;
-    const ub = lb + BATCH_SIZE;
+    const ub = Math.min(i * (BATCH_SIZE) + BATCH_SIZE, lb + BATCH_SIZE);
     promises.push(
       new Promise(resolve => resolve(getPrimesSumAux(lb, ub)))
     );
@@ -14,22 +16,21 @@ export async function getPrimesSum(upperBound) {
 }
 
 function getPrimesSumAux(lowerBound, upperBound) {
-  let i = lowerBound;
   let sum = 0;
-
-  while (i < upperBound) {
+  for (let i = lowerBound; i < upperBound; i++) {
     if (isPrime(i)) sum += i;
-    i++;
   }
   return sum;
 }
 
 export function isPrime(n) {
-  let i = 2;
-  while (i < n && n % i !== 0) i++;
-  return i === n;
+  const root = Math.sqrt(n)
+  for (let i = 2; i <= root; i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 getPrimesSum(2_000_000).then(res => {
   console.log(res);
-})
+});
