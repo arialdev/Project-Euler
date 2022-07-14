4fc3e8dafbefb4f45cc28d86b6e0e67f13762594
Álvaro <alvarora12@gmail.com>
2022-07-14 19:21:29 +0200

    15

---
+++ js/problems/15.js
@@ -0,0 +1,13 @@
+const findPaths = (n) => {
+  return findPathsAux(n, 0, 0);
+};
+
+const findPathsAux = (n, r, c) => {
+  if (r === n && c === n) {
+    return 1;
+  }
+  if (r > n || c > n) return 0;
+  return findPathsAux(n, r + 1, c) + findPathsAux(n, r, c + 1);
+}
+
+console.log(findPaths(20))
