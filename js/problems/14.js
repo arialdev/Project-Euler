f719f51ce4bcbf56b7822e1f080c137c55b887ea
Álvaro <alvarora12@gmail.com>
2022-07-14 12:56:50 +0200

    14

---
+++ js/problems/14.js
@@ -0,0 +1,21 @@
+export const findLongestCollatzSequence = () => {
+  let result = { n: undefined, length: -1 };
+  for (let i = 0; i < 1_000_000; i++) {
+    const seq = collatzSequence(i).length;
+    if (seq > result.length) {
+      result = { n: i, length: collatzSequence(i).length };
+    }
+  }
+  return result;
+}
+
+const collatzSequence = (n) => {
+  const collatzRecursive = (n, sequence) => {
+    if (n <= 1) return sequence;
+    if (n % 2 === 0) return collatzRecursive(n / 2, [...sequence, n]);
+    else return collatzRecursive(3 * n + 1, [...sequence, n]);
+  };
+  return collatzRecursive(n, []);
+};
+
+console.log(findLongestCollatzSequence());
