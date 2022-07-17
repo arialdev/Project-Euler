6423626c29373c28304ed31369f38538d18c3c32
Álvaro <alvarora12@gmail.com>
2022-07-17 20:29:26 +0200

    improve 15

--- js/problems/15.js
+++ js/problems/15.js
@@ -3,9 +3,7 @@ const findPaths = (n) => {
 };
 
 const findPathsAux = (n, r, c) => {
-  if (r === n && c === n) {
-    return 1;
-  }
+  if (r === n && c === n) return 1;
   if (r > n || c > n) return 0;
   return findPathsAux(n, r + 1, c) + findPathsAux(n, r, c + 1);
 }
