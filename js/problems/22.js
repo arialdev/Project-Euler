276503ab2c21711f1c552de1040586b818032bfb
arialdev <alvarora12@gmail.com>
2022-07-22 17:05:24 +0200

    22

---
+++ js/problems/22.js
@@ -0,0 +1,16 @@
+import { readFileSync } from 'node:fs';
+import { resolve } from 'node:path'
+
+const getNameListFromURI = uri => {
+  return readFileSync(uri, { encoding: 'utf-8' })
+    .split(',')
+    .map(w => w.slice(1, -1))
+    .sort((a, b) => a.localeCompare(b))
+    .map(w => [...w]
+      .map(c => c.charCodeAt(0) - 64)
+      .reduce((acc, c) => acc + c)
+    )
+    .reduce((acc, e, i) => acc + e * (i + 1));
+};
+
+console.log(getNameListFromURI(resolve('files/p022_names.txt')))
