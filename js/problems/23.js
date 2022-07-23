9c7356e504252495be552e195278bdf8f30c8d43
arialdev <alvarora12@gmail.com>
2022-07-23 14:33:19 +0200

    23

---
+++ js/problems/23.js
@@ -0,0 +1,33 @@
+function ex23() {
+  const divisorsMap = {};
+  for (let i = 2; i <= 28123; i++) {
+    let value = 0;
+    for (let j = 1; j < i; j++) {
+      if (i % j === 0) {
+        value += j;
+      }
+    }
+    divisorsMap[i] = value;
+  }
+  const abundants = Object
+    .entries(divisorsMap)
+    .filter(n => n[1] > +n[0])
+    .map(n => +n[0]);
+
+  const abundantsSum = new Set();
+  for (let i = 0; i < abundants.length; i++) {
+    for (let j = i; j < abundants.length; j++) {
+      abundantsSum.add(abundants[i] + abundants[j]);
+    }
+  }
+
+  let result = 0;
+  for (let i = 0; i <= 28123; i++) {
+    if (!abundantsSum.has(i)) result += i;
+  }
+  return result;
+}
+
+console.time('ex23');
+console.log(ex23());
+console.timeEnd('ex23');
