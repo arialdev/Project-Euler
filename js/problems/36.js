b7669c57a82cb0521900668345b3a70b81e6e489
arialdev <alvarora12@gmail.com>
2022-07-24 14:00:23 +0200

    36

---
+++ js/problems/36.js
@@ -0,0 +1,20 @@
+const ex36 = () => {
+  let acc = 0;
+  for (let i = 0; i < 1_000_000; i++) {
+    if (isPalindrome(i) && isPalindrome(i.toString(2))) acc += i;
+  }
+  return acc;
+}
+
+const isPalindrome = n => {
+  const str = n.toString();
+  let i = 0;
+  let j = str.length - 1;
+  let result = true;
+  while (i < j) {
+    if (str[i++] !== str[j--]) return false;
+  }
+  return result;
+}
+
+console.log(ex36())
