eb827de15a9283e85c420cdd7fa5110de57ad6dc
Álvaro <alvarora12@gmail.com>
2022-07-25 12:55:06 +0200

    56

---
+++ js/problems/56.js
@@ -0,0 +1,13 @@
+const ex56 = () => {
+  let result = 0;
+  for (let i = 1; i < 100; i++) {
+    for (let j = 1; j < 100; j++) {
+      result = Math.max(result, (BigInt(i) ** BigInt(j)).toString().split('').map(d => +d).reduce((acc, d) => acc + d));
+    }
+  }
+  return result;
+}
+
+console.time('ex56');
+console.log(ex56());
+console.timeEnd('ex56');
