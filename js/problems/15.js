3c30f2fae4a60186d54d7c6934e029b47d5a0582
arialdev <alvarora12@gmail.com>
2022-07-30 02:31:14 +0200

    improve ex15 performance

--- js/problems/15.js
+++ js/problems/15.js
@@ -1,11 +1,18 @@
-const findPaths = (n) => {
-  return findPathsAux(n, 0, 0);
-};
+export const ex15 = (n = 20, m = 20) => {
+  const cache = {};
 
-const findPathsAux = (n, r, c) => {
-  if (r === n && c === n) return 1;
-  if (r > n || c > n) return 0;
-  return findPathsAux(n, r + 1, c) + findPathsAux(n, r, c + 1);
+  const findPathsAux = (r, c, n, m) => {
+    if (r === n && c === m) {
+      cache[`${r}_${c}`] = 1;
+      return 1;
+    }
+    if (r > n || c > m) {
+      cache[`${r}_${c}`] = 0;
+      return 0;
+    }
+    const left = cache[`${r + 1}_${c}`] ??= findPathsAux(r + 1, c, n, m);
+    const right = cache[`${r}_${c + 1}`] ??= findPathsAux(r, c + 1, n, m);
+    return right + left;
+  }
+  return findPathsAux(0, 0, n, m)
 }
-
-export const ex15 = findPaths.bind(this, 20);
