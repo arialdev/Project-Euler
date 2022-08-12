a73535ec6a0228e8f829b67ecb58dcce0ee40059
Álvaro <alvarora12@gmail.com>
2022-08-12 10:06:45 +0200

    52

---
+++ js/problems/52.js
@@ -0,0 +1,17 @@
+export const ex52 = () => {
+  let i = 1;
+  while (true) {
+    let m = 2;
+    while (m <= 6) {
+      if (!containsSameDigits(i, m * i)) break;
+      if (m === 6) return i;
+      m += 2;
+    }
+    i++;
+  }
+};
+
+const containsSameDigits = (a, b) => {
+  const stringify = n => [...n.toString()].sort().join('');
+  return stringify(a) === stringify(b);
+}
